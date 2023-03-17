50bd15f03ace893fb10d6a9aebae23518d7d6d78
MateuszKar2 <system505@protonmail.com>
2023-03-17 16:03:38 +0100

    changes

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,12 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryCardsSet = createGallery(galleryItems);
+
+function createGallery(galleryItems) {
+    return galleryItems
+    .map(({ original, preview, description}) => {
+        return `<div class="gallery__item" style= "border-radius: 5%; box-shadow: inset 2px 2px 2px 0px rgba(255, 255, 255, 0.5),
+        7px 7px 20px 0px rgba(0, 0, 0, 0.1), 4px 4px 5px 0px rgba(0, 0, 0, 0.1)">
+}
