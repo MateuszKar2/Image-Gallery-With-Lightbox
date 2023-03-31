cc3fedc9e3d90601638c142a53f080749ecac5fa
MateuszKar2 <system505@protonmail.com>
2023-03-31 22:55:09 +0200

    add description

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,24 +2,24 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 const gallery = document.querySelector(".gallery");
-// const markup = createItemsMarkup(galleryItems);
-
-
-
-
-// function createItemsMarkup(item) {
-// return galleryItems
-//    .map(({ preview, original, description }) => {
-//      return `<div class="gallery__item">
-//  <a class="gallery__link" href="${original}">
-//  <img
-//  class="gallery__image"
-//  src="${preview}"
-//  data-source="${original}"
-//  alt="${description}"/></a></div>`;})
-//      .join("");
-  
-//  }
+// -------------------------
+//Pobieram galerię za pomocą querySelector
+//Wykorzystuję metodę .map, celem otrzymania przekształconej kopii tablicy
+//Parametrami metody map. są: "preview"-mały obraz, "original"-duży obraz, "description"-opis obrazka.
+//Implementuje gotowy szablon gotowych tagów HTML/CSS wprowadzając zmienne
+//Łącze elementy tablicy w string za pomocą metody .join
+//Za pomocą metody .insert...("beforeend") dodaje ciąg znaków wewnątrz elementu po wszystkich dzieciach
+//Stosuje detektor zdarzeń(nazwę zdarzenia/ f. wywołania zwrotnego/ -)
+//Funkcja wywołania zwrotnego(z pierwszym argumentem-"event")
+//Kasuje domyślne działanie przeglądarki za pomocą metody dla eventu tj.preventDefault();
+//Stosuje warunek "if", i "event.target"
+//Przy pomocy .nodeName sprawdzam typ elementu
+//Wykorzystanie biblioteki "BASICLIGHTBOX" 
+//Przypisuje swojej instancji(basicLightbox) zmienną
+//Opiera się na prametrach- widoczny i zamknięty + modyfikuje zawartość(basicLightbox)
+//Wykorzystanie warunku "if" przypisującego escape do zamknięcia obrazka
+//Wykorzystanie metody .show
+//-------------------------
 
 
 const markup = galleryItems.map(({ preview, original, description }) => {
@@ -33,7 +33,7 @@ const markup = galleryItems.map(({ preview, original, description }) => {
      .join("");
   
  
-gallery.insertAdjacentHTML("beforeend", markup);
+ gallery.insertAdjacentHTML("beforeend", markup);
 
 
  gallery.addEventListener('click', selectGalleryItem);
