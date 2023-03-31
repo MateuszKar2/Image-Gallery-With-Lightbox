0f6f985b702d35baa85683faa5a72cc579c6cde9
MateuszKar2 <system505@protonmail.com>
2023-03-31 21:34:23 +0200

    add second.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,24 +2,27 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 const gallery = document.querySelector(".gallery");
-const  markup = createItemsMarkup(galleryItems);
+ const markup = createItemsMarkup(galleryItems);
+ gallery.insertAdjacentHTML("beforeend", markup);
 
-gallery.insertAdjacentElement("beforeend", markup);
 
-function createItemsMarkup(item) {
-    return galleryItems
-    .map(({ preview, original, description}) => {
-        return `<div class="gallery__item">
-    <a class="gallery__link" href="${original}">
-    <img  
-    class="gallery__image"
-    src="${preview}"
-    alt="${description}"/></a></div>`;})
-    .join(""); 
-}
 
-const lighbox = new SimpleLightbox('.gallery a', {
-    caption: true,
-    captionsData: 'alt',
-    captionDelay: 50,
+ function createItemsMarkup(item) {
+ return galleryItems
+   .map(({ preview, original, description }) => {
+     return `<div class="gallery__item">
+ <a class="gallery__link" href="${original}">
+ <img
+ class="gallery__image"
+ src="${preview}"
+ alt="${description}"/></a></div>`;})
+     .join("");
+  
+ }
+
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  caption: true,
+  captionsData: 'alt',
+  captionDelay: 50,
 });
